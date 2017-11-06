a5a3f0146618bad046ee3efe43d0b48b2c3b7e3c
gslu <lushiguang@aliyun.com>
2017-11-06 20:15:06 +0800

    .

--- mysite/blog/migrations/0019_auto_20171104_1131.py
+++ mysite/blog/migrations/0019_auto_20171104_1131.py
@@ -15,39 +15,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Book',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='books',
-                                           to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.AlterField(
-            model_name='postclass',
-            name='post',
-            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='blog.Post'),
-        ),
-        migrations.AlterField(
-            model_name='postclass',
-            name='post_type',
-            field=models.CharField(
-                choices=[('self', '\u539f\u521b'), ('reprint', '\u8f6c\u8f7d'), ('collect', '\u6536\u85cf')],
-                default='self', max_length=10),
-        ),
-        migrations.AlterField(
-            model_name='postclass',
-            name='user',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='classes',
-                                    to=settings.AUTH_USER_MODEL),
-        ),
-        migrations.AddField(
-            model_name='post',
-            name='book',
-            field=models.ForeignKey(blank=True, default=None, on_delete=django.db.models.deletion.CASCADE,
-                                    related_name='posts', to='blog.Book'),
-        ),
     ]
