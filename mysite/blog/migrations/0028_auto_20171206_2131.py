381cc020fac9a9a8edadc47662f059ad3206af01
gslu <lushiguang@aliyun.com>
2018-05-12 10:43:59 +0800

    优化

--- mysite/blog/migrations/0028_auto_20171206_2131.py
+++ mysite/blog/migrations/0028_auto_20171206_2131.py
@@ -12,8 +12,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RenameModel(
-            old_name='EmailVerifyRecord',
-            new_name='VerifyRecord',
-        ),
+        #migrations.RenameModel(
+            #old_name='EmailVerifyRecord',
+            #new_name='VerifyRecord',
+        #),
     ]
